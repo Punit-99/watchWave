d92c8fbaf915e17a4e9c2b2e773a106f642559cd
Punit Soni <sonipunit006@gmail.com>
2025-03-09 21:59:00 +0530

    c4

--- client/src/components/admin/final/tabs/ShowBasicDetails.jsx
+++ client/src/components/admin/final/tabs/ShowBasicDetails.jsx
@@ -6,8 +6,6 @@ export const ShowBasicDetails = ({ showDetails, setShowDetails }) => {
   return (
     <div className="flex-1 p-4 bg-gray-700 rounded-md">
       <h2 className="text-lg font-semibold text-white mb-4">Details</h2>
-      {console.log(showDetails)}
-      {console.log(setShowDetails)}
       <AdminForm
         formControls={FINAL_showBasicFormControls}
         formData={showDetails}
