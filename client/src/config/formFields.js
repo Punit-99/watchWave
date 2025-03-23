fc68e06ef571a172e3ece7a5d23a1f423f3e13db
Punit Soni <sonipunit006@gmail.com>
2025-03-23 15:06:39 +0530

    .

--- client/src/config/formFields.js
+++ client/src/config/formFields.js
@@ -113,8 +113,8 @@ export const FINAL_initialState = {
   title: "DBZ",
   description:
     "Dragon Ball Z follows the adventures of Goku who, along with the Z Warriors, defends the Earth against evil. The action adventures are entertaining and reinforce the concept of good versus evil. Dragon Ball Z teaches valuable character virtues such as teamwork, loyalty, and trustworthiness.",
-  poster: null, // Assuming file input
-  thumbnail: null, // Assuming file input
+  poster: null,
+  thumbnail: null,
   category: "",
   genre: ["action", "adventure"],
   releaseDate: "04/04/2025",
