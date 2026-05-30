e0c4bea4892d25ce3c5dd37a9cf89a939d894ac8
Punit Soni <sonipunit006@gmail.com>
2026-05-30 20:20:27 +0530

    reset

--- prisma/migrations/20260528062736_init_watchwave/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `language` column on the `Content` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Content" DROP COLUMN "language",
-ADD COLUMN     "language" TEXT[];
